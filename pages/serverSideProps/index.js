d3b0d6ac4fdfe667077f1277b139b1a196324723
aryanmajeed <aryanmajid97@gmail.com>
2022-02-21 02:08:06 +0300

    add staticProps header

--- pages/serverSideProps/index.js
+++ pages/serverSideProps/index.js
@@ -2,13 +2,16 @@ import Link from "next/link";
 
 export default function serverSideProps({ posts }) {
   return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.title}>
-          <Link href={"/serverSideProps/" + post.id}>{post.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div>Got static HTML page using getStaticProps feature</div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.title}>
+            <Link href={"/serverSideProps/" + post.id}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
